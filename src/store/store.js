1123628a761695eab874b828e088a4eb1939b08d
hataevicha5922 <hataevicha5922@gmail.com>
2022-03-20 14:10:08 +0300

    get data from api

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-
+import { movieReducer } from './movie';
 import { userReducer } from './user';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  movies: movieReducer,
 });
 
 export const store = configureStore({
--- src/store/user/slice.js
+++ src/store/user/slice.js
@@ -4,13 +4,18 @@ const userSlice = createSlice({
   name: 'user',
   initialState: {
     isAuthenticated: false,
+    userEmail: '',
+    password: '',
   },
   reducers: {
     setAuthenticated(state, action) {
       state.isAuthenticated = action.payload;
     },
+    setUserEmail(state, action) {
+      state.userEmail = action.payload;
+    },
   },
 });
 
 export const userReducer = userSlice.reducer;
-export const { setAuthenticated } = userSlice.actions;
+export const { setAuthenticated, setUserEmail } = userSlice.actions;
